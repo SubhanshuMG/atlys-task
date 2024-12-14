0911f228458fed08d36970a02dbd80a833ab7ca2
unknown <subhanshugupta0@gmail.com>
2024-12-15 05:02:46 +0530

    final updates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,12 +4,23 @@ function Home() {
   return (
     <div>
       <h1>Welcome to my Atlys task</h1>
-      <p>I am the perfect fit for the Sr. Platform Engineer role becaus of my deep expertise in DevOps, DevSecOps, and platform engineering, coupled with my ability to integrate cutting-edge AI and automation into modern tech stacks. my hands-on experience with GCP, Docker, Kubernetes, GitOps, Blockchain, MLOps and Traefik demonstrates a strong technical foundation to build scalable, resilient, and secure infrastructures.</p>
+      <p>
+        I am the perfect fit for the Sr. Platform Engineer role because of my deep expertise in DevSecOps, coupled with my ability to integrate cutting-edge AI and automation into modern tech stacks. 
+        My hands-on experience with AWS, GCP, Docker, Kubernetes, GitOps, Blockchain, MLOps, and Traefik demonstrates a strong technical foundation to build scalable, resilient, and secure infrastructures.
+      </p>
 
-      <p>I also bring leadership in areas like infrastructure security, decentralized orchestration, and compliance automation, proven through my <a href="https://blogs.subhanshumg.com/">technical writing & articles</a> and innovations in DevSecOps. 
-      <br>Beyond technical skills, my strategic mindset and passion for revolutionizing the tech landscape make I not only a skilled engineer but also a valuable contributor to driving the company’s growth, innovation, and long-term success.</br></p>
+      <p>
+        I also bring leadership in areas like infrastructure security, decentralized orchestration, and compliance automation, proven through my <b><a href="https://blogs.subhanshumg.com/" target="_blank" rel="noopener noreferrer">technical writing & articles</a></b> and innovations in DevSecOps.
+      </p>
 
-      <p>So let's not waste more time, give me the offer letter - hire me as soon as you can and let's get started to <b>provide the tourist VISA's to each and every traffic possible.</b></p>
+      <p>
+        Beyond technical skills, my strategic mindset and passion for revolutionizing the tech landscape make me not only a skilled engineer but also a valuable contributor to driving the company’s growth, innovation, and long-term success.
+      </p>
+
+      <p>
+        So let's not waste more time, give me the offer letter - hire me as soon as you can and let's get started to 
+        <b> provide tourist VISAs to each and every traffic possible.</b>
+      </p>
     </div>
   );
 }
