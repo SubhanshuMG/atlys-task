60ca0f22b1d0cbaecc84ba4662a540d0f1eb560b
unknown <subhanshugupta0@gmail.com>
2024-12-15 04:56:11 +0530

    updates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,7 +4,12 @@ function Home() {
   return (
     <div>
       <h1>Welcome to my Atlys task</h1>
-      <p>Please give me the offer letter :).</p>
+      <p>I am the perfect fit for the Sr. Platform Engineer role becaus of my deep expertise in DevOps, DevSecOps, and platform engineering, coupled with my ability to integrate cutting-edge AI and automation into modern tech stacks. my hands-on experience with GCP, Docker, Kubernetes, GitOps, Blockchain, MLOps and Traefik demonstrates a strong technical foundation to build scalable, resilient, and secure infrastructures.</p>
+
+      <p>I also bring leadership in areas like infrastructure security, decentralized orchestration, and compliance automation, proven through my <a href="https://blogs.subhanshumg.com/">technical writing & articles</a> and innovations in DevSecOps. 
+      <br>Beyond technical skills, my strategic mindset and passion for revolutionizing the tech landscape make I not only a skilled engineer but also a valuable contributor to driving the company’s growth, innovation, and long-term success.</br></p>
+
+      <p>So let's not waste more time, give me the offer letter - hire me as soon as you can and let's get started to <b>provide the tourist VISA's to each and every traffic possible.</b></p>
     </div>
   );
 }
